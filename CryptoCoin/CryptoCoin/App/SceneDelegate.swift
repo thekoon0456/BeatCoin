0981ea83a834c9376b8c0239ec20229e2aa229e6
Deokhun KIM <thekoon0456@gmail.com>
2024-02-27 14:59:52 +0900

    [Feat] 프로젝트 기본 세팅

---
+++ CryptoCoin/CryptoCoin/App/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  CryptoCoin
+//
+//  Created by Deokhun KIM on 2/27/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+        
+        let vc = CoinSearchViewController()
+        window?.rootViewController = vc
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
