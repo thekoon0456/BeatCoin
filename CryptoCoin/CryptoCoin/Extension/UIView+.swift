04af20c91dde81079a11045b7e8908c95b1a2b0a
Deokhun KIM <thekoon0456@gmail.com>
2024-02-27 15:04:59 +0900

    [Feat] Extension 추가

---
+++ CryptoCoin/CryptoCoin/Extension/UIView+.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+.swift
+//  CryptoCoin
+//
+//  Created by Deokhun KIM on 2/27/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ subviews: UIView...) {
+        for subview in subviews {
+            addSubview(subview)
+        }
+    }
+}
---
+++ CryptoCoin/CryptoCoin/Utility/Observable.swift
@@ -0,0 +1,44 @@
+//
+//  Observable.swift
+//  CryptoCoin
+//
+//  Created by Deokhun KIM on 2/27/24.
+//
+
+import Foundation
+
+final class Observable<T> {
+    
+    // MARK: - Properties
+    
+    typealias Listener = (T) -> Void
+    
+    private var listener: Listener?
+    
+    private var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    var currentValue: T {
+        value
+    }
+    
+    // MARK: - Lifecycles
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    // MARK: - Helpers
+    
+    func bind(listener: Listener?) {
+        listener?(value)
+        self.listener = listener
+    }
+    
+    func onNext(_ value: T) {
+        self.value = value
+    }
+}
